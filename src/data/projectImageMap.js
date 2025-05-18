93600463f00064dc32a295a1ebc38cf2143ea0f8
Aman <rayblue748@gmail.com>
2025-05-18 12:53:21 +0530

    updated image and fix card

--- src/data/projectImageMap.js
+++ src/data/projectImageMap.js
@@ -1,11 +1,15 @@
 import tableau from '../assets/projects/tableau2.jpeg';
 import netflix from '../assets/projects/netflix.jpeg';
 import sql from '../assets/projects/sql.jpg';
+import aerofit from '../assets/projects/aerofit.png';
+import hospital from '../assets/projects/hospital.jpg';
 
 const projectImageMap = {
   "projects/tableau2.jpeg": tableau,
   "projects/netflix.jpeg": netflix,
   "projects/sql.jpg": sql,
+  "projects/aerofit.png": aerofit,
+  "projects/hospital.jpg": hospital,
 };
 
 export default projectImageMap;
